813c4a9834deea3398ef5f8714ca9901657fcb13
Colin Rupp <colin@thinkful.com>
2018-11-26 09:28:56 -0800

    adds JWT secret and expiry

--- config.js
+++ config.js
@@ -7,10 +7,7 @@ module.exports = {
   process.env.DATABASE_URL || 'mongodb://dev:password123@ds149382.mlab.com:49382/spaced-repetition',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://dev:password123@ds125851.mlab.com:25851/spaced-repetition-test'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+  'mongodb://dev:password123@ds125851.mlab.com:25851/spaced-repetition-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.envJWT_EXPIRY || '4d'
 };
