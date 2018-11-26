4e704c1846fc97c7c9fb81a413dbe183ab9afe64
Colin Rupp <colin@thinkful.com>
2018-11-26 08:44:43 -0800

    adds mlab database

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  process.env.DATABASE_URL || 'mongodb://dev:password123@ds149382.mlab.com:49382/spaced-repetition',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+        'mongodb://dev:password123@ds125851.mlab.com:25851/spaced-repetition-test'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
